7c0bda1335d18fb60650556024634589e3ed6d29
Denis Morozov <denis.moroz.morozov@gmail.com>
2018-11-09 16:47:42 +0300

    Init iOS client

---
+++ m-ios-client/m-ios-client/m-ios-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  m-ios-client
+//
+//  Created by Denis Morozov on 09.11.2018.
+//  Copyright © 2018 Denis Morozov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
