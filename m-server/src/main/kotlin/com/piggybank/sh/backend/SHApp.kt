107f09cfa32c09c0454d561115f0e8f127a7ee47
frikadelki <alexander.kazanin@gmail.com>
2018-11-10 18:52:38 +0300

    Shelter entities.

--- m-server/src/main/kotlin/com/piggybank/sh/backend/SHApp.kt
+++ m-server/src/main/kotlin/com/piggybank/sh/backend/SHApp.kt
@@ -1,7 +1,10 @@
 package com.piggybank.sh.backend
 
+import com.piggybank.sh.generated.ShelterMapObject
 import org.jetbrains.exposed.sql.Database
 
 class SHApp(db: Database) {
     private val repo = SHRepo(db)
+
+    fun shelterMapObjects(): Iterable<ShelterMapObject> = repo.shelterMapObjects()
 }
