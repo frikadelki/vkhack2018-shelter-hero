d00f234564e494885049cebdae4fb84fc6f7ea1f
frikadelki <alexander.kazanin@gmail.com>
2018-11-11 04:13:36 +0300

    ShelterQuestRecord

---
+++ m-server/src/main/kotlin/com/piggybank/sh/backend/db/QuestRecord.kt
@@ -0,0 +1,40 @@
+package com.piggybank.sh.backend.db
+
+import com.piggybank.sh.generated.ShelterQuest
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Table
+import sh.generated.ShelterQuestRecordOuterClass
+
+object QuestRecordTable : IntIdTable() {
+    val quest = text("quest")
+
+    val startTimeEpochMillis = long("start_time_epoch_millis")
+
+    val status = text("status")
+}
+
+class QuestRecordEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<QuestRecordEntity>(SheltersTable)
+
+    var startTimeEpochMillis by QuestRecordTable.startTimeEpochMillis
+
+    var quest by ProtobufMessageDelegator<ShelterQuest>(QuestRecordTable.quest) { ShelterQuest.newBuilder() }
+
+    private var _status by QuestRecordTable.status
+    
+    var status: ShelterQuestRecordOuterClass.ShelterQuestRecordStatus
+        get() = ShelterQuestRecordOuterClass.ShelterQuestRecordStatus.valueOf(_status)
+        set(value) {
+            _status = value.name
+        }
+
+    var demands by OrderDemandEntity via QuestRecordsDoneDemands
+}
+
+object QuestRecordsDoneDemands : Table() {
+    val questRecord = reference("quest_record", QuestRecordTable).primaryKey(0)
+    val demand = reference("demand", OrdersDemandsTable).primaryKey(1)
+}
