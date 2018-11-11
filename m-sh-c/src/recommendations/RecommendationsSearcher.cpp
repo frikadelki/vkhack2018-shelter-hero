f023d2d378cf7067a3d94ba318110c861eebce4d
dkmike <dkmike@gmail.com>
2018-11-11 05:32:13 +0300

    Sort recommendations

--- m-sh-c/src/recommendations/RecommendationsSearcher.cpp
+++ m-sh-c/src/recommendations/RecommendationsSearcher.cpp
@@ -2,19 +2,34 @@
 
 #include "OrderPlanner.h"
 
+#include <tuple>
+#include <algorithm>
+
 namespace sh
 {
     Recommendations findRecommendations(const Task& task)
     {
-        Recommendations recommendations;
 
+        std::vector<Trip> trips;
         for (const Order& order : task.orders())
         {
             const Opt<Trip> trip = planOrder(order, task.params());
             if (trip)
             {
-                recommendations.mutable_recommendations()->Add()->CopyFrom(trip.value());
+                trips.emplace_back(trip.value());
             }
+        }
+
+        sort(begin(trips), end(trips), [](const Trip& lhs, const Trip& rhs)
+        {
+            return std::pair(lhs.stats().timespent(), lhs.stats().distancetraveled())
+                <  std::pair(rhs.stats().timespent(), rhs.stats().distancetraveled());
+        });
+
+        Recommendations recommendations;
+        for (const Trip& trip : trips)
+        {
+            recommendations.mutable_recommendations()->Add()->CopyFrom(trip);
         }
 
         return recommendations;
